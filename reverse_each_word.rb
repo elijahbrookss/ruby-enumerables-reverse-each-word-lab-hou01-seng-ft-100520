c72bd8e097502381fb91704efcbb0909f5d11b6d
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 15:54:31 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word (string)
+  word_array = string.split();
+  new_string_array = [];
+  word_array.each_with_index{
+    |word, index|
+    
+    char_array = word.split("").join.reverse
+    new_string_array.push(char_array)
+    
+    if not (index == word_array.length - 1) then
+      new_string_array.push(" ")
+    end
+  }
+  new_string_array
+end
+
+reverse_each_word("Hello World!")
