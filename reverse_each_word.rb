7242af01734b0ab1043bdbb98d00808a32836ec7
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 16:01:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word (string)
     char_array.push(word.split("").join.reverse)
   end
   char_array.collect{|word|
-    word = word + " "
+    word = word + "i"
   }
   p char_array
 end
