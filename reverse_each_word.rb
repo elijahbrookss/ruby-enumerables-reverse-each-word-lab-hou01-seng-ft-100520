9986536467cb10be32f53e003a5d3d7717a98c11
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 15:59:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (string)
   word_array = string.split();
   char_array = []
   for word in word_array do
-    char_array = word.split("").join.reverse
+    char_array.push(word.split("").join.reverse)
   end
   p char_array
   word_array.collect{|word|
